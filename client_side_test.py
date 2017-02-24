e766f2620322adadd42cee934b1fff0e57f08ae5
Harshal Godhia <hsgodhia@gmail.com>
2017-02-23 21:30:54 -0500

    new test case

--- client_side_test.py
+++ client_side_test.py
@@ -1,7 +1,7 @@
 import requests
 
 test404 = requests.request('GET', 'https://cdorstats.herokuapp.com/afsdasdasf')
-assert str(test404.json()) == r"""{'action': 'change the endpoint to /reddit_stats/subreddit/<subr> ', 'message': "you've come to 404!"}"""
+assert str(test404.json()) == r"""{'action': 'change the endpoint to /reddit_stats/subreddit/<subr> ', 'message': "You've come to 404!"}"""
 
 test_invalid_subreddit = requests.request('GET', 'https://cdorstats.herokuapp.com/reddit_stats/subreddit/leardgsdgsdg')
 assert str(test_invalid_subreddit.json()) == r"""{'action': 'change subreddit to something else', 'message': '0 results found for given subreddit'}"""
@@ -11,3 +11,6 @@ assert str(test_invalid_queryparam_rank_by.json()) == r"""{'action': 'rank is ho
 
 test_invalid_queryparam_limit = requests.request('GET', 'https://cdorstats.herokuapp.com/reddit_stats/subreddit/news?rank_by=top&limit=a')
 assert str(test_invalid_queryparam_limit.json()) == r"""{'action': 'limit is a number try again!', 'message': 'invalid limit query param'}"""
+
+test_ok = requests.request('GET', 'https://cdorstats.herokuapp.com/reddit_stats/subreddit/news?limit=1')
+assert "'subreddit_name': 'news'" in str(test_ok.json())

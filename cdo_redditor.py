56ce1a4b7ce447e8f46ec3521ef24b336fd7066e
Harshal Godhia <hsgodhia@gmail.com>
2017-02-23 21:13:35 -0500

    cdo code review

--- cdo_redditor.py
+++ cdo_redditor.py
@@ -1,62 +1,76 @@
 from flask import Flask, jsonify, request, redirect
-from collections import OrderedDict
-import praw
+import praw, os, redis
 
-reddit = praw.Reddit(client_id='p8Hs4qPd7HD1Wg',client_secret='9rEQJxxC7Tp_DD8wQLsshAOudNU', user_agent='App by /u/hershey92')
+#redis_url = os.environ('REDIS_URL')
+redis_url = 'redis://h:p560e0fb7875b26e8cd0567e6b885f7077d0889f8444afceb834556832bb6ce63@ec2-34-194-51-203.compute-1.amazonaws.com:33939'
+redis_conn = redis.from_url(redis_url)
+
+client_id = redis_conn.get('CLIENT_ID').decode("utf-8") 
+client_secret = redis_conn.get('CLIENT_SECRET').decode("utf-8") 
+user_agent = redis_conn.get('USER_AGENT').decode("utf-8") 
+
+reddit_inst = praw.Reddit(client_id = client_id, client_secret = client_secret , user_agent = user_agent)
 reddapp = Flask(__name__)
 
 class Error:
-    def make_error(self, status_code, sub_code, message, action):
-        response = jsonify({'status': status_code,'sub_code': sub_code,'message': message,'action': action})
+    def make_error(self, status_code, message, action):
+        response = jsonify({'message': message,'action': action})
         response.status_code = status_code
         return response
 
 @reddapp.errorhandler(404)
 def not_found(error):
     err = Error()
-    return err.make_error(200, 2, "...coded overnight, you've come to 404!", "change the endpoint to /reddit_stats/subreddit/<subr> ")
+    return err.make_error(404, "You've come to 404!", "change the endpoint to /reddit_stats/subreddit/<subr> ")
 
 @reddapp.route('/reddit_stats/subreddit/<subr>', methods=['GET'])
 def getSubredditSubmissions(subr):
     err = Error()
     rank_by = request.args.get('rank_by', 'hot')
-    k = request.args.get('limit', '10')
-    f_resp = OrderedDict()
-    result = []
+    results_limit = request.args.get('limit', '10')
+    response_object = {}
+    articles = []
+    try:
+        subreddit_found = reddit_inst.subreddit(subr)
+        response_object['subreddit_name'] = subr
+        response_object['subreddit_title'] = subreddit_found.title
+    except Exception:
+        return err.make_error(202, "0 results found for given subreddit", "change subreddit to something else")
     
     try:
-        subrredit_found = reddit.subreddit(subr)
-        f_resp[subr + ' is about'] = subrredit_found.title
+        results_limit = int(results_limit)
     except Exception:
-        return err.make_error(200, 1, "No results found for given subreddit", "change subreddit to something else")
-    
-    try:
-        k = int(k)
-    except Exception:
-        return err.make_error(200, 3, "invalid limit query param", "limit is a number try again!")
+        return err.make_error(400, "invalid limit query param", "limit is a number try again!")
     
     listing = None
     if rank_by == 'hot':
-        listing = subrredit_found.hot(limit=k)
+        listing = subreddit_found.hot(limit=results_limit)
     elif rank_by == 'new':
-        listing = subrredit_found.new(limit=k)
+        listing = subreddit_found.new(limit=results_limit)
     elif rank_by == 'top':
-        listing = subrredit_found.top(limit=k)
+        listing = subreddit_found.top(limit=results_limit)
     elif rank_by == 'rising':
-        listing = subrredit_found.rising(limit=k)
+        listing = subreddit_found.rising(limit=results_limit)
     else:
-        return err.make_error(200, 3, "invalid rank query param", "rank is hot/new/rising/top only try again!")
-    
+        return err.make_error(400, "invalid rank query param", "rank is hot/new/rising/top only try again!")
+
     for submission in listing:
-        r = {}
-        r['title'] = submission.title
-        r['score'] = submission.score
-        r['Link'] = submission.url
-        result.append(r)
-    if len(result) < 1:
-        return err.make_error(200, 0, "0 results found for given ranking type", "change rank type to something else")
-    f_resp[rank_by + " " + str(k)] = result
-    return jsonify(f_resp)
+        story = {}
+        story['title'] = submission.title
+        story['score'] = submission.score
+        story['link'] = submission.url
+        articles.append(story)
+
+    if len(articles) < 1:
+        return err.make_error(202, "0 results found for given ranking type", "change rank type to something else")
+    
+    response_object['results_limit'] = str(results_limit)
+    response_object['ranking_method'] = rank_by
+    response_object['stories'] = articles
+
+    http_response = jsonify(response_object)
+    http_response.status_code = 200
+    return http_response
 
 if __name__ == '__main__':
     reddapp.run(debug=True)
--- client_side_test.py
+++ client_side_test.py
@@ -1,13 +1,13 @@
 import requests
 
 test404 = requests.request('GET', 'https://cdorstats.herokuapp.com/afsdasdasf')
-assert str(test404.json()) == r"""{'action': 'change the endpoint to /reddit_stats/subreddit/<subr> ', 'message': "...coded overnight, you've come to 404!", 'status': 200, 'sub_code': 2}"""
+assert str(test404.json()) == r"""{'action': 'change the endpoint to /reddit_stats/subreddit/<subr> ', 'message': "you've come to 404!"}"""
 
 test_invalid_subreddit = requests.request('GET', 'https://cdorstats.herokuapp.com/reddit_stats/subreddit/leardgsdgsdg')
-assert str(test_invalid_subreddit.json()) == r"""{'action': 'change subreddit to something else', 'message': 'No results found for given subreddit', 'status': 200, 'sub_code': 1}"""
+assert str(test_invalid_subreddit.json()) == r"""{'action': 'change subreddit to something else', 'message': '0 results found for given subreddit'}"""
 
 test_invalid_queryparam_rank_by = requests.request('GET', 'https://cdorstats.herokuapp.com/reddit_stats/subreddit/news?rank_by=afadgagadf')
-assert str(test_invalid_queryparam_rank_by.json()) == r"""{'action': 'rank is hot/new/rising/top only try again!', 'message': 'invalid rank query param', 'status': 200, 'sub_code': 3}"""
+assert str(test_invalid_queryparam_rank_by.json()) == r"""{'action': 'rank is hot/new/rising/top only try again!', 'message': 'invalid rank query param'}"""
 
 test_invalid_queryparam_limit = requests.request('GET', 'https://cdorstats.herokuapp.com/reddit_stats/subreddit/news?rank_by=top&limit=a')
-assert str(test_invalid_queryparam_limit.json()) == r"""{'action': 'limit is a number try again!', 'message': 'invalid limit query param', 'status': 200, 'sub_code': 3}"""
+assert str(test_invalid_queryparam_limit.json()) == r"""{'action': 'limit is a number try again!', 'message': 'invalid limit query param'}"""
